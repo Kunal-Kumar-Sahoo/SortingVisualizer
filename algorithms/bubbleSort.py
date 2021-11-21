9cce62ed2c33405557a23a48f8ac9739bd560555
Kunal Kumar Sahoo <kunal.sahoo2003@gmail.com>
2021-11-21 13:44:04 +0530

    Implemented bubble sort algorithm

---
+++ algorithms/bubbleSort.py
@@ -0,0 +1,14 @@
+import time # Create some time difference between each comparison
+
+from colors import *
+
+def bubble_sort(data, drawData, timeTick):
+    size = len(data)
+    for i in range(size-1):
+        for j in range(size-i-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j] // swapping elements
+                drawData(data, [YELLOW if x == j or x == j+1 else BLUE for x in range(len(data))])
+                time.sleep(timeTick)
+    
+    drawData(data, [BLUE for x in range(len(data))])
